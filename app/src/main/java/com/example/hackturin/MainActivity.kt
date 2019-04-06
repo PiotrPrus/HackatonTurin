907bf9206b76cc0e767f76a9df403ec95c0406bf
elia <migliorelia@gmail.com>
2019-04-06 19:10:29 +0200

    implemented method for setting settings in SharedPreferences

--- app/src/main/java/com/example/hackturin/MainActivity.kt
+++ app/src/main/java/com/example/hackturin/MainActivity.kt
@@ -1,12 +1,35 @@
 package com.example.hackturin
 
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.hackturin.feature.settings.SettingsFragment
+import android.content.Context.BIND_AUTO_CREATE
+import android.content.Intent
+
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Create new fragment and transaction
+        val settingsFragment = SettingsFragment()
+        val transaction = supportFragmentManager.beginTransaction()
+
+        // Replace whatever is in the fragment_container view with this fragment,
+        // and add the transaction to the back stack if needed
+        transaction.replace(R.id.nav_host_fragment, settingsFragment)
+        transaction.addToBackStack(null)
+
+        // Commit the transaction
+        transaction.commit()
+
     }
+
 }
--- app/src/main/java/com/example/hackturin/feature/settings/SettingsFragment.kt
+++ app/src/main/java/com/example/hackturin/feature/settings/SettingsFragment.kt
@@ -1,34 +1,94 @@
 package com.example.hackturin.feature.settings
 
+import android.content.Context
+import android.os.Build
 import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
+import android.preference.PreferenceManager
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.lifecycle.Observer
+import android.widget.CompoundButton
+import android.widget.SeekBar
+import android.widget.Switch
 
+import android.widget.SeekBar.OnSeekBarChangeListener
 import com.example.hackturin.R
+
 
 class SettingsFragment : Fragment() {
 
     private lateinit var viewModel: SettingsViewModel
+    private lateinit var switchRecorded: Switch
+    private lateinit var switchTrack: Switch
+    private lateinit var seekRange: SeekBar
+    private lateinit var contextActivity: Context
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.settings_fragment, container, false)
+
+        var view = inflater.inflate(R.layout.settings_fragment, container, false)
+
+        switchRecorded = view.findViewById(R.id.switchHistoryRecorded)
+        switchTrack = view.findViewById(R.id.switchTripMode)
+        seekRange = view.findViewById(R.id.seekBarRange)
+
+
+
+        return view
+    }
+
+    override fun onAttach(context: Context?) {
+        this.contextActivity = context!!
+        super.onAttach(context)
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProviders.of(this).get(SettingsViewModel::class.java)
-        // TODO: Use the ViewModel
-        viewModel.currentRange.observe(this, Observer { sentThiIntToFirebase(it) })
+        reloadValue()
+        setupListeners()
     }
 
-    private fun sentThiIntToFirebase(it: Int?) {
+
+    private fun reloadValue(){
+        var preferences = PreferenceManager.getDefaultSharedPreferences(context)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            seekRange.setProgress(preferences.getInt("range",40),true)
+        }
+
+        switchTrack.setChecked(preferences.getBoolean("tracking", true))
+        switchRecorded.setChecked(preferences.getBoolean("recording", true))
+    }
+
+    private fun setupListeners() {
+        switchRecorded.setOnCheckedChangeListener{ button,checked -> saveValuePreference("recording", checked ) }
+        switchTrack.setOnCheckedChangeListener{ button,checked -> saveValuePreference("tracking", checked ) }
+        seekRange.setOnSeekBarChangeListener(object : OnSeekBarChangeListener {
+            override fun onStopTrackingTouch(seekBar: SeekBar) {}
+            override fun onStartTrackingTouch(seekBar: SeekBar) {}
+            override fun onProgressChanged(seekBar: SeekBar, progress: Int, fromUser: Boolean) {
+                saveValuePreference("range",progress)
+            }
+        })
+    }
+
+    private fun saveValuePreference(name: String,value: Any){
+        var preferences = PreferenceManager.getDefaultSharedPreferences(context).edit()
+
+        if ( value is Boolean ) {
+            preferences.putBoolean(name,value)
+        }
+
+        if ( value is Int ) {
+            preferences.putInt(name,value)
+        }
+
+        preferences.apply()
 
     }
 
